cefd54c623e64d58c0236e3df9cba2c0a26b3821
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2020-10-14 20:58:39 +0800

    Create optimized_approach.cpp

---
+++ optimized_approach.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+void primeFact(int n) {
+	for(int i=2; i*i<=n; ++i)
+		if(n%i==0) {
+			int cnt=0;
+			while(n%i==0) {
+				cnt++;
+				n/=i;
+			}
+			cout << i << " ^ " << cnt << "\n";
+		}
+	if(n>1)
+		cout << n << " ^ "  << 1 << "\n";
+}
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	int n;
+	cin >> n;
+	primeFact(n);
+}
